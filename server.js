137b430dec3f174e3d0f6009359ee2198ae68e76
shalzmsc-workspace <shalzmsc@gmail.com>
2026-04-29 15:17:53 +0530

    fix mongodb connection error

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ mongoose.connect(
 .then(() => console.log("✅ MongoDB Connected"))
 .catch((err) => console.log("❌ DB Error:", err));
 
-
 // ================= MODELS =================
 const userSchema = new mongoose.Schema({
   name: String,
@@ -193,4 +192,13 @@ const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log("🚀 Server running on port", PORT);
+});
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    console.log("❌ USERS ERROR:", err);
+    res.status(500).json({ error: "Server error" });
+  }
 });
