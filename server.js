551d3be204b9af6d83f965deb9dea1a68d5b8089
shalzmsc-workspace <shalzmsc@gmail.com>
2026-04-29 23:46:03 +0530

    one

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors());
 
 // ================= ROOT =================
