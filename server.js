4e9fa17e55e64bfac5f388f4cd4f5ed6e441a2fc
shalzmsc-workspace <shalzmsc@gmail.com>
2026-04-29 13:16:12 +0530

    add root route

--- server.js
+++ server.js
@@ -182,4 +182,7 @@ const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log("🚀 Server running on port", PORT);
+});// ROOT CHECK (VERY IMPORTANT)
+app.get("/", (req, res) => {
+  res.send("🚀 MindBridge Backend Running");
 });
