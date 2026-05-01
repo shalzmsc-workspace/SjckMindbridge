5769b1b2da772607b7139b25df9b290773908a7b
shalzmsc-workspace <shalzmsc@gmail.com>
2026-05-01 11:46:04 +0530

    ooo

--- server.js
+++ server.js
@@ -244,3 +244,12 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log("🚀 Server running on port", PORT);
 });
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find(); // MongoDB
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+});
